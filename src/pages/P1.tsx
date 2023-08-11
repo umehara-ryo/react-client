66078a30019bb6874af347a661ce41d8a9756dc6
umehara <a603751058@gmail.com>
2023-08-11 21:08:38 +0800

    reactプロジェクト 1.タグにイベントを加える

--- src/pages/P1.tsx
+++ src/pages/P1.tsx
@@ -1,13 +1,17 @@
-
-
 import Student from "../model/Student";
 
-export default function P1({student} : {student : Student}){
+export default function P1({student}: { student: Student }) {
 
-    return(
+    function handleClick(e: React.MouseEvent) {
+        alert("七瀬最高");
+        console.log(e);
+
+    }
+
+    return (
         <div>
-        <h1>{student.name}</h1>
-        <h2>{student.id}</h2>
+            <h1 onClick={handleClick}>{student.name}</h1>
+            <h2>{student.id}</h2>
             <p>年齢は{student.age} 性別は{student.sex}
             </p>
         </div>
