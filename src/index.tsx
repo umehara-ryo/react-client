2abfb85f08b800f05b7b3d79dc74d50588385ecb
umehara <a603751058@gmail.com>
2023-08-13 12:19:52 +0800

    reactプロジェクト 1.フォーマット

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import P1 from "./pages/P1";
 import P2 from "./pages/P2";
 import P3 from "./pages/P3";
 import P8 from "./pages/P8";
+import A1 from "./pages/A1";
 
 const stu1 = {id : 1,name : "umehara", sex : "男",age : 20};
 const stu2 = {id : 1,name : "umehara", sex : "男",age : 20};
@@ -22,7 +23,7 @@ root.render(
       <Hello msg={"七瀬最高"}/>
       <P1 student={stu1}></P1>d
       <P2　students={students} hideAge={true}></P2>*/}
-      <P8></P8>
+      <A1></A1>
 
   </React.StrictMode>
 );
---
+++ src/pages/A1.tsx
@@ -0,0 +1,7 @@
+
+    import { Button } from "antd";
+
+    export default function A1() {
+
+        return <Button type='primary'>按钮</Button>
+    }
